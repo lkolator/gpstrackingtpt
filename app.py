c0397386a5d08f4fe8bcdc1285e7bc418056ae7d
Łukasz Kolator <lukasz.kolator@enigma.com.pl>
2015-10-30 11:00:09 +0100

    - zmiana na str losowych wartosci - na posta odpowiadam z losowa wartoscia configa

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import random
 
 FILE_FORMAT = '%Y_%m_%d_%H_%M_%S'
 
-#"{"srn":"00000000","lat":"00000.00000X","lon":"00000.00000X","utc":"000000.00","acc":"00000","flg":"0000"}";
+# "{"srn":"00000000","lat":"00000.00000X","lon":"00000.00000X","utc":"000000.00","acc":"00000","flg":"0000"}";
 # htr - HTTP tracking; 0 - off, 1 - on
 # str - SMS tracking; 0 - off, 1 - on
 # tpr - tracking period; 0 - 15s, 1 - 1min, 2 - 5min, 3 - 30min
@@ -29,13 +29,13 @@ FILE_FORMAT = '%Y_%m_%d_%H_%M_%S'
 # prt - HTTP reporting port number
 # {"srn":"00000000","htr":"X","str":"X","tpr":""X","pho":"+000000000000","www":"0000000000000000000000000000000000000000","prt":"00000"}"
 config = {
-        'htr':'1',
-        'str':'1',
-        'tpr':'1',
-        'pho':'+48509386813'
+        'htr': '1',
+        'str': '1',
+        'tpr': '1',
+        'pho': '+48509386813'
         }
 
-cfg = { 'cfg':'1' }
+cfg = {'cfg': '1'}
 
 app = Flask(__name__)
 
@@ -43,10 +43,11 @@ app = Flask(__name__)
 last_time = 0
 counter = 0
 
+
 def random_config():
-    config['htr'] = random.randint(0,1)
-    config['str'] = random.randint(0,1)
-    config['tpr'] = random.randint(0,3)
+    config['htr'] = str(random.randint(0, 1))
+    config['str'] = str(random.randint(0, 1))
+    config['tpr'] = str(random.randint(0, 3))
 
 
 class Decoder(json.JSONDecoder):
@@ -74,12 +75,12 @@ class Decoder2(json.JSONDecoder):
 class Main(MethodView):
     def get(self):
         return "OK"
-        #return render_template('index.html', devices=devices)
+        # return render_template('index.html', devices=devices)
 
 
 class Hello(MethodView):
     def get(self):
-        #return "OK"
+        # return "OK"
         random_config()
         return Response(json.dumps(config),  mimetype='application/json')
 
@@ -89,7 +90,8 @@ class Hello(MethodView):
             gps = json.loads(request.data, cls=Decoder)
             results = Geocoder.reverse_geocode(gps['lat'], gps['lon'])
             print results
-            #return unicode(results[0])
+            # return unicode(results[0])
+            cfg['cfg'] = str(radnom.randint(0, 1))
             return Response(json.dumps(cfg),  mimetype='application/json')
         except:
             return "OK"
@@ -105,5 +107,5 @@ app.add_url_rule('/hello', view_func=Hello.as_view('hello'))
 app.add_url_rule('/hello/<string:device_id>', view_func=HelloPost.as_view('hellopost'))
 
 if __name__ == '__main__':
-    #app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    # app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(debug=True, host='0.0.0.0', port=8111)
